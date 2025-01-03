05c3b6910194421f239387fb2797a1ed4752b55c
sp3c7r4 <sp3c7r40x00@gmail.com>
2025-01-04 00:16:39 +0100

    Day 3

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Bro")
+	var conferenceName = "Go conference"
+	const conferenceTickets = 50
+	fmt.Println("Welcome to", conferenceName, "booking application")
+	fmt.Println(conferenceTickets)
+	fmt.Println("Get your tickets here to attend")
+
+	fmt.Println(conferenceName)
 }
