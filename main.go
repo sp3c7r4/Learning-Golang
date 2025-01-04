8c6f9a4d73b507d6636e0f4902605542a3f2d674
sp3c7r4 <sp3c7r40x00@gmail.com>
2025-01-04 14:03:28 +0100

    Working with Arrays and Slices

--- main.go
+++ main.go
@@ -3,11 +3,14 @@ package main
 import "fmt"
 
 func main() {
-
+	conferenceName := "Go conference"
 	var firstName string;
 	var lastName string;
 	var email string;
 	var userTickets int;
+	var remainingTickets uint = 50;
+	//Array type
+	var bookings []string;
 	
 	//Asking for user Input
 	fmt.Println("Enter your first name: ")
@@ -18,7 +21,15 @@ func main() {
 	fmt.Scan(&email)
 	fmt.Println("Enter the number of tickets you wanna buy: ")
 	fmt.Scan(&userTickets)
+	
+	remainingTickets = remainingTickets - uint(userTickets)
+	bookings = append(bookings, firstName + " " + lastName)
+	fmt.Printf("The whole array: %v\n", bookings)
+	fmt.Printf("The first value: %v\n", bookings[0])
+	fmt.Printf("Array Type: %T\n", bookings)
+	fmt.Printf("Array Size: %T\n", len(bookings))
+	
 	// Formating Variables
-	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v \n", firstName, lastName, userTickets, email)
-
+	fmt.Printf("Thank you %v for booking %v tickets. You will recieve a confirmation email at %v \n", bookings[0], userTickets, email)
+	fmt.Printf("%v tickets remaining for %v\n", remainingTickets, conferenceName)
 }
