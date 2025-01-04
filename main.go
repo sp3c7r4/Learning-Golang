37d63b5bebb0e060c4027a6ef045a9468cea2ab5
sp3c7r4 <sp3c7r40x00@gmail.com>
2025-01-04 13:17:03 +0100

    Recieveing user's input

--- main.go
+++ main.go
@@ -3,17 +3,22 @@ package main
 import "fmt"
 
 func main() {
-	var conferenceName = "Go conference"
-	const conferenceTickets = 50
-	fmt.Printf("Welcome to %v booking application\n", conferenceName)
-	fmt.Println(conferenceTickets)
-	fmt.Println("Get your tickets here to attend")
-	fmt.Println(conferenceName)
 
-	var userName string;
+	var firstName string;
+	var lastName string;
+	var email string;
 	var userTickets int;
 	
-	userName = "Tom"
-	userTickets = 9
-	fmt.Printf("User %v booked %v tickers. \n", userName, userTickets)
+	//Asking for user Input
+	fmt.Println("Enter your first name: ")
+	fmt.Scan(&firstName)
+	fmt.Println("Enter your last name: ")
+	fmt.Scan(&lastName)
+	fmt.Println("Enter your email: ")
+	fmt.Scan(&email)
+	fmt.Println("Enter the number of tickets you wanna buy: ")
+	fmt.Scan(&userTickets)
+	// Formating Variables
+	fmt.Printf("Thank you %v %v for booking %v tickets. You will recieve a confirmation email at %v \n", firstName, lastName, userTickets, email)
+
 }
